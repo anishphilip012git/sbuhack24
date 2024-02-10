c299ed45cd98aa120610770510858d573f34090a
Anish Philip <anishphilip012git@live.in>
2024-02-10 16:32:19 -0500

    audio chanegs

--- uicode/firebase-app/src/components/AudioRecorder.js
+++ uicode/firebase-app/src/components/AudioRecorder.js
@@ -13,9 +13,10 @@ const AudioRecorder = () => {
   };
 
   const startRecording = async () => {
+    console.log("start")
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const options = { mimeType: 'audio/ogg' };
+      const options = { mimeType: 'audio/webm' };
       const newRecorder = new MediaRecorder(stream, options, workerOptions);
       newRecorder.addEventListener('dataavailable', (e) => {
         if (audioRef.current) {
@@ -30,16 +31,18 @@ const AudioRecorder = () => {
   };
 
   const stopRecording = () => {
+    console.log("stop")
     if (recorder) {
       recorder.stop();
+      console.log(audioRef.audio);
       setRecorder(null);
     }
   };
 
   return (
     <div>
-      <button onClick={startRecording} id="startBtn">Start Recording</button>
-      <button onClick={stopRecording} id="stopBtn">Stop Recording</button>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={startRecording} id="startBtn">Start Recording</button>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={stopRecording} id="stopBtn">Stop Recording</button>
       <audio ref={audioRef} controls id="player"></audio>
     </div>
   );
