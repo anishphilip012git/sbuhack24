59737d6dbe7c1dfd8c4c567bbd8a66f3da411fac
PrathameshBagal <prathameshbagal2908@gmail.com>
2024-02-10 23:35:58 -0500

    Login.js push before api calling

--- uicode/firebase-app/src/pages/login.js
+++ uicode/firebase-app/src/pages/login.js
@@ -33,24 +33,24 @@ const login = () => {
     }
     return (
         <>
-            <div class=" bg-white dark:bg-slate-700 rounded-3xl shadow relative mt-20 p-6 justify-center w-1/2 mx-auto my-auto">
+            <div className=" bg-white dark:bg-slate-700 rounded-3xl shadow relative mt-20 p-6 justify-center w-1/2 mx-auto my-auto">
 
-                <form class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" onSubmit={handleSubmit} action="/">
-                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Login to our platform</h3>
+                <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" onSubmit={handleSubmit} action="/">
+                    <h3 className="text-xl font-medium text-gray-900 dark:text-white">Login to our platform</h3>
                     <div>
-                        <label  class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your email</label>
-                        <input onChange={e=>setEmail(e.target.value)} type="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="" />
+                        <label  className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your email</label>
+                        <input onChange={e=>setEmail(e.target.value)} type="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="" />
                     </div>
                     <div>
-                        <label for="password" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your password</label>
-                        <input onChange={e=> setPassword(e.target.value)} type="password" name="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
+                        <label for="password" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your password</label>
+                        <input onChange={e=> setPassword(e.target.value)} type="password" name="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
                     </div>
               
-                    <button type="submit" class="hover:scale-110  dark:text-slate-700 bg-black w-full  dark:bg-white  text-base font-medium px-4 py-2 rounded-md mt-12">
+                    <button type="submit" className="hover:scale-110  dark:text-slate-700 bg-black w-full  dark:bg-white  text-base font-medium px-4 py-2 rounded-md mt-12">
                         Login to your account
                     </button>
-                    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        Not registered? <a href="" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                        Not registered? <a href="" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                     </div>
                 </form>
             </div>
